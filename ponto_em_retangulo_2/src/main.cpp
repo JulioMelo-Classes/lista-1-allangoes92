1c86712193eaa348c4ef78d00d3c73ad0709366a
allangoes92 <57724201+allangoes92@users.noreply.github.com>
2021-06-25 20:23:40 -0300

    Add files via upload

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -2,6 +2,7 @@
  * @brief Implementação do Ponto em Retângulo V2.
  * @author selan
  * @data June, 6th 2021
+ * INSIDE=0, BORDER=1, OUTSIDE=2
  */
 
 #include <iostream>
@@ -14,14 +15,57 @@ using std::max;
 
 #include "function.h"
 
-//=== Funções
-
-// Coloque aqui qualquer função auxiliar que desejar.
-
+void verifica ( Ponto &A, Ponto &B)
+{
+    Ponto aux;
+    if (A.x > B.x)
+    {
+        aux.x = A.x;
+        A.x = B.x;
+        B.x = aux.x;
+        aux.y = A.y;
+        A.y = B.y;
+        B.y = aux.y;        
+    }
+    else if (A.y > B.y)
+    {
+        aux.y = A.y;
+        A.y = B.y;
+        B.y = aux.y;        
+    }
+}
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+     Ponto IE, SD, P;
 
+    while( cin >> std::ws >> IE.x)
+    {
+        std::cin >> IE.y;
+        std::cin >> SD.x;
+        std::cin >> SD.y;
+        verifica (IE, SD);
+        std::cin >> P.x;
+        std::cin >> P.y;
+        if (IE.x == SD.x || IE.y == SD.y)
+        {
+            std::cout << "invalid" << endl;
+        }
+        else
+        {
+            switch (pt_in_rect( IE, SD, P ))
+            {
+            case 0:
+                std::cout << "inside" << endl;
+                break;
+            case 1:
+                std::cout << "border" << endl;
+                break;
+            case 2:
+                std::cout << "outside" << endl;
+                break;
+            }
+        }
+    }
     return 0;
 }
